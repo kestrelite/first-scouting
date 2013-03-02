e2636e625661c1e134d7a937bf2810cd0a0edf42
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-03-02 05:27:16 +0000

    QuickSort check this guys

--- trunk/FIRSTScouting/src/tournamentftc/QuickSort.java
+++ trunk/FIRSTScouting/src/tournamentftc/QuickSort.java
@@ -1,36 +1,53 @@
 package tournamentftc;
 
-public class QuickSort {
+import java.util.Arrays;
 
-    public void sort(double[] array) {
+public class QuickSort {
+    
+    private static int[] index;
+
+    public static void sort(double[] array) {
         sort(array, 0, array.length - 1);
     }
 
-    public void sort(double[] array, int begin, int end) {
+    public static void sort(double[] array, int begin, int end) {
+        index = new int[array.length];
         if (begin < end) {
             int pivotIndex = (begin + end) / 2;
-            int pos = organize(array, begin, end, pivotIndex);
+            int pos = partition(array, begin, end, pivotIndex);
             sort(array, begin, pos - 1);
             sort(array, pos + 1, end);
         }
     }
 
-    private void swap(double[] array, int i, int j) {
+    private static void swap(double[] array, int i, int j) {
         double t = array[i];
         array[i] = array[j];
         array[j] = t;
+        System.out.println("i: "+i);
+        System.out.println("j: "+j);
+        index[j] = i;
+        System.out.println(index[j]);
     }
 
-    private int organize(double[] array, int begin, int end, int pivotIndex) {
-        swap(array, pivotIndex, end);
-        int pos = begin;
-        for (int i = begin; i < end; i++) {
-            if (array[i] < array[end]) {
-                swap(array, i, pos);
+    private static int partition(double[] a, int left, int right, int pivotIndex) {
+        swap(a, pivotIndex, right);
+        int pos = left;
+        for (int i = left; i < right; i++) {
+            if (a[i] < a[right]) {
+                swap(a, i, pos);
                 pos++;
             }
         }
-        swap(array, end, pos);
+        swap(a, right, pos);
         return pos;
+    }
+    
+    public static void main(String [] args) {
+        double[] d = {0.0, 80.0, 80.0, 10.0, 50.0, 70.0, 60.0, 90.0, 20.0, 30.0, 40.0, 0.0};
+        System.out.println(Arrays.toString(d));
+        sort(d, 0, d.length-1);
+        System.out.println(Arrays.toString(d));
+        System.out.println(Arrays.toString(index));
     }
 }
