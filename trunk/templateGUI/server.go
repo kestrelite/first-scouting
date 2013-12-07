605506e21763bb0ed28b22b3cab5b3b95a6ead0b
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-12-07 05:20:30 +0000

    Fixed server.go

--- trunk/templateGUI/server.go
+++ trunk/templateGUI/server.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/ftc", handler)
+	http.Handle("/", http.FileServer(http.Dir("C:/Users/Yousuf/Desktop/first-scouting/templateGUI")))
 	panic(http.ListenAndServe(":4278", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	f, _ := ioutil.ReadFile("template.html")
-	fmt.Fprintf(w, bytes.NewBuffer(f).String())
+	w.Header().Set("Content-Type", "text/html")
+
+	//fmt.Fprintf(w, bytes.NewBuffer(f).String())
+	io.WriteString(w, bytes.NewBuffer(f).String())
 }
