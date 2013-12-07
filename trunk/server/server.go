1b8c6ed4d9549081dbf1f0bb585c79bbf69df0d0
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-12-07 06:17:38 +0000

    Server is now all on root. ServeMux

--- trunk/server/server.go
+++ trunk/server/server.go
@@ -5,24 +5,24 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-/*func main() {
-	http.HandleFunc("/ftc", handler)
-	http.Handle("/", http.FileServer(http.Dir("C:/Users/Yousuf/Desktop/first-scouting/templateGUI")))
-	panic(http.ListenAndServe(":4278", nil))
-}*/
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	f, _ := ioutil.ReadFile("C:/Users/Yousuf/Desktop/first-scouting/server/template.html")
-	w.Header().Set("Content-Type", "text/html")
-
-	//fmt.Fprintf(w, bytes.NewBuffer(f).String())
-	io.WriteString(w, bytes.NewBuffer(f).String())
-}
+var chttp = http.NewServeMux()
 
 func StartServer() {
-	http.HandleFunc("/ftc", handler)
-	http.Handle("/", http.FileServer(http.Dir("C:/Users/Yousuf/Desktop/first-scouting/server")))
-	panic(http.ListenAndServe(":4278", nil))
+	chttp.Handle("/", http.FileServer(http.Dir("C:/Users/Yousuf/Desktop/first-scouting/server")))
+
+	http.HandleFunc("/", handler) // homepage
+	http.ListenAndServe(":4278", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.URL.Path, ".") {
+		chttp.ServeHTTP(w, r)
+	} else {
+		f, _ := ioutil.ReadFile("C:/Users/Yousuf/Desktop/first-scouting/server/template.html")
+		w.Header().Set("Content-Type", "text/html")
+		io.WriteString(w, bytes.NewBuffer(f).String())
+	}
 }
