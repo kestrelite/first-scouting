8a98e0a48543fd652812071aa1c403029c6e5a53
yousufmsoliman@gmail.com <yousufmsoliman@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-12-07 05:38:21 +0000

    magic

---
+++ marshal.go
@@ -0,0 +1,28 @@
+package marshal
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+func MarshalTeams(Teams map[int]Team) string {
+	mapstr := `[`
+	i := 0
+	for _, v := range Teams {
+		val, _ := json.Marshal(v)
+		mapstr += bytes.NewBuffer(val).String()
+		if i != len(Teams)-1 {
+			mapstr += ","
+		}
+		i++
+	}
+	mapstr += `]`
+	return mapstr
+}
+
+func UnmarshalMatches(j []byte) Match {
+	var m Match
+	json.Unmarshal(j, &m)
+	return m
+}
