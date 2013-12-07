ef28c4c34f3a8c22a62946c2b67971f7d5cced3e
kiansheik3128@gmail.com <kiansheik3128@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-12-07 07:28:00 +0000

    This is a match Get

--- trunk/server/server.go
+++ trunk/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	//"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,15 +15,35 @@ func StartServer() {
 	chttp.Handle("/", http.FileServer(http.Dir("server")))
 
 	http.HandleFunc("/", handler) // homepage
-	http.ListenAndServe(":4278", nil)
+	panic(http.ListenAndServe(":4278", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if strings.Contains(r.URL.Path, ".") {
 		chttp.ServeHTTP(w, r)
 	} else {
-		f, _ := ioutil.ReadFile("server/template.html")
-		w.Header().Set("Content-Type", "text/html")
-		io.WriteString(w, bytes.NewBuffer(f).String())
+		r.ParseForm()
+		if r.Method == "POST" {
+			if r.Form.Get("type") == "match" {
+				//matchJson := r.Form.Get("data")
+
+			} else if r.Form.Get("type") == "team" {
+				//teamJson := r.Form.Get("data")
+			}
+		} else if r.Method == "GET" {
+			w.Header().Set("Content-Type", "application/json")
+
+			if r.Form.Get("type") == "team" {
+				//Marshal Team Struct
+				io.WriteString(w, "This is a get team")
+			} else if r.Form.Get("type") == "match" {
+				//Marshal Match Struct
+				io.WriteString(w, "This is a Match Get")
+			}
+		} else {
+			f, _ := ioutil.ReadFile("server/template.html")
+			w.Header().Set("Content-Type", "text/html")
+			io.WriteString(w, bytes.NewBuffer(f).String())
+		}
 	}
 }
