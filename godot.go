7dfc6dc23c5767b5ab486586adc37552cca9a0ee
noahsutsmo@gmail.com <noahsutsmo@gmail.com@0a85f6a0-e1f1-926b-40ee-97b640866937>
2013-12-07 02:28:58 +0000

    Added preliminary calculation

--- godot.go
+++ godot.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"math"
+	"math"
 )
 
 type Team struct {
@@ -32,17 +32,44 @@ func UpdateTeamList(matchList []Match, oldList map[int]Team) map[int]Team {
 
 func PrelimCalc(matchList []Match, teamList map[int]Team) map[int]Team {
 	type TeamTempDat struct {
-		Sum, QP, RP int
+		Played, Sum int
+		QP, RP      int
 	}
-	teamDatList := make(map[int]TeamTempDat)
+	teamDatMap := make(map[int]TeamTempDat)
 
 	for q := 0; q < len(matchList); q++ {
-		for t := 0; q < len(matchList[q].TeamNums); t++ {
+		for t := 0; t < len(matchList[q].TeamNums); t++ {
 			var teamnum = matchList[q].TeamNums[t]
+			var teamdat = teamDatMap[teamnum]
+			teamdat.Played++
 			var match = matchList[q]
-			//teamDatList[teamnum].Sum += func() { matchList[q].RScore }
+
+			teamdat.RP += int(math.Min(float64(match.RScore), float64(match.BScore)))
+			if q < 3 {
+				teamdat.Sum += match.RScore - match.RPen
+				if match.RScore > match.BScore {
+					teamdat.QP += 2
+				}
+			} else {
+				teamdat.Sum += match.BScore - match.BPen
+				if match.BScore > match.RScore {
+					teamdat.QP += 2
+				}
+			}
+
+			teamDatMap[teamnum] = teamdat
 		}
 	}
+
+	for i, v := range teamDatMap {
+		var tmp = teamList[i]
+		tmp.AvgScore = float32(v.Sum) / float32(v.Played)
+		tmp.QP = float32(v.QP)
+		tmp.RP = float32(v.RP)
+		teamList[i] = tmp
+	}
+
+	return teamList
 }
 
 func main() {
@@ -50,8 +77,9 @@ func main() {
 	tL := make(map[int]Team)
 
 	mL = append(mL, Match{TeamNums: []int{1, 2, 3, 4}, RScore: 50, BScore: 30, RPen: 0, BPen: 0})
-	mL = append(mL, Match{TeamNums: []int{6, 2, 7, 4}, RScore: 50, BScore: 30, RPen: 0, BPen: 0})
+	mL = append(mL, Match{TeamNums: []int{6, 2, 7, 4}, RScore: 30, BScore: 50, RPen: 0, BPen: 0})
 	tL = UpdateTeamList(mL, tL)
+	tL = PrelimCalc(mL, tL)
 
 	fmt.Println(mL)
 	fmt.Println(tL)
